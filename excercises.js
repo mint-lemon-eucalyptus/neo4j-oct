3ed9082c3169e7a0c395b73c72bba513c390bae8
green-dev <ya.dim.tk@gmail.com>
2013-12-09 15:20:41 +0400

    authorization

---
+++ excercises.js
@@ -0,0 +1,14 @@
+//each excercise must have interval of allowed index numbers of nodes and relations
+module.exports=[
+    {
+        using:{
+            nmin:0,
+            nmax:100,
+            rmin:0,
+            rmax:100
+        },
+        json:{
+        },
+        question:'excerc'
+    }
+];
